bdce37ad90f644ab22be1ffe0b1ff62b82578b83
H M Moniruzzaman <neermoniruzzaman@yahoo.com>
2021-05-09 17:29:45 +0200

    Added user routes tests

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -15,7 +15,7 @@ def test_user_registration(client, mock_mongo):
     """
     Given a flask application
     When the '/api/v1/user/register/' route is called
-    Then check that a '200' status code is returned.
+    Then check that valid response is returned.
     """
     request = {
         "username": "testuser",
@@ -24,5 +24,34 @@ def test_user_registration(client, mock_mongo):
     response = client.post(
         '/api/v1/user/register/',
         data=request
+        )
+    assert response.status_code == 200
+
+
+def test_user_login(client, mock_mongo):
+    """
+    Given a flask application
+    When the '/api/v1/user/login/' route is called
+    Then check that valid response is returned.
+    """
+    request = {
+        "username": "testuser",
+        "password": "secret"
+    }
+    response = client.post(
+        '/api/v1/user/login/',
+        data=request
+        )
+    assert response.status_code == 200
+
+
+def test_user_logout(client, mock_mongo):
+    """
+    Given a flask application
+    When the '/api/v1/user/logout/' route is called
+    Then check that valid response is returned.
+    """
+    response = client.get(
+        '/api/v1/user/logout/'
         )
     assert response.status_code == 200
